c4a810cd0d7bc711bfba5ac16ea09aed55206a34
minGom0302 <qazwsx636@naver.com>
2022-11-17 15:52:33 +0900

    git commit -m "Create main bottom navigation customizing"

--- app/src/main/java/com/example/coffeetrip/Activity_Main.java
+++ app/src/main/java/com/example/coffeetrip/Activity_Main.java
@@ -1,14 +1,46 @@
 package com.example.coffeetrip;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Activity_Main extends AppCompatActivity {
+    private static final String TAG = "Main_Activity";
+    private BottomNavigationView botNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botNavView = findViewById(R.id.main_bottom_navigation);
+
+        // 첫 화면으로 fragment_main_home.xml 띄움
+        getSupportFragmentManager().beginTransaction().add(R.id.main_frame_container, new Fragment_main_home()).commit();
+
+        // Bot Menu 선택에 따른 Fragment 화면 띄움움
+       botNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.main_bottom01 :
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_container, new Fragment_main_home()).commit();
+                        break;
+                    case R.id.main_bottom02 :
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_container, new Fragment_main_magnifier()).commit();
+                        break;
+                    case R.id.main_bottom03 :
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_container, new Fragment_main_favorite()).commit();
+                        break;
+                    case R.id.main_bottom04 :
+                        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_container, new Fragment_main_mypage()).commit();
+                }
+                return true;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/coffeetrip/Fragment_main_mypage.java
@@ -0,0 +1,19 @@
+package com.example.coffeetrip;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_main_mypage extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main_mypage, container, false);
+        return view;
+    }
+}
